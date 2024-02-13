aaa23582d947d87aef2227bbf3a047602d399c54
Bytestorm <31070777+Bytestorm5@users.noreply.github.com>
2024-02-12 23:02:39 -0500

    refactor degreeworks scrape

--- dw_prereq_scrape.py
+++ dw_prereq_scrape.py
@@ -7,60 +7,90 @@ import datetime
 import json
 
 
+dw_majors = ['APPH', 'ARCH', 'BIOC', 'BNFO', 'BIOL', 'BMED', 'BINF', 'BIOP', 'BIS', 'BUS', 'CHE', 'CHM', 'CE', 'COMM', 'CSCI', 'COE', 'CS', 'CBUS', 'CIM', 'CPSY', 'DSCO', 'DSSO', 'EE', 'ESC', 'EVSC', 'FTEC', 'FRSC', 'GEN', 'HCI', 'ID', 'IE', 'IS', 'IT', 'MFEN', 'MTEN', 'MATH', 'ME', 'STS', 'UDCC', 'UDEN', 'UDSL', 'WIS']
+
+
 session = requests.Session()
 
 # Set initial cookies from DegreeWorks
 initial_cookies = ...
+id_8digit = ...
 session.cookies.update(initial_cookies)
 
+def get_course_info(discipline, number):
+    global session
+    response = session.get(f'https://dw-prod.ec.njit.edu/responsiveDashboard/api/course-link?discipline={discipline}&number={number}&=')
+    return response.json()
 
-courses = set()
+def get_major(major, catalogYear):
+    if major not in dw_majors:
+        raise ValueError("Invalid Major")
+    headers = {
+        "studentId":str(id_8digit),
+        "isIncludeInprogress":False,
+        "isIncludePreregistered":False,
+        "isKeepCurriculum":False,
+        "school":"U",
+        "degree":"BS",
+        "catalogYear":"2022",
+        "goals":[{"code":"MAJOR","value":str(major),"catalogYear":str(catalogYear)}],
+        "classes":[]
+    }
+    url = "https://dw-prod.ec.njit.edu/responsiveDashboard/api/audit"
+    response = session.get(url, headers=headers)
     
-year = 2024
-while year > 2010:
-    for sem in [SemesterType.SPRING, SemesterType.SUMMER, SemesterType.FALL, SemesterType.WINTER]:
-        term = NJIT.term_code(year, sem)
-        subjects = NJIT.get_subjects(term)
+    robj = response.json()
+    relevant_blocks = [x for x in robj['blockArray'] if x['requirementType'] in ['OTHER', 'MAJOR', 'MINOR']]
+    return relevant_blocks
+
+if __name__ == "__main__":
+    courses = set()
         
-        db = mongo_client["NJIT_Course_API"]
-        collection = db["Courses"]
-        
-        for s in subjects:
-            subject = s['SUBJECT']
-            sections = NJIT.get_sections(term, subject)
-            print(f"[{term}]: {subject} ({len(courses)})")
-            for sec in sections:
-                course: str = sec['COURSE']
-                if course in courses:
-                    continue
-                else:      
-                    c1 = course.replace(' ', '')
-                    c2 = split_course_code(c1)
-                    
-                    cname = " ".join(c2)
-                    
-                    if collection.find_one({'_id':cname}) != None:
-                        print("Found " + cname)
-                        courses.add(course)
+    year = 2024
+    while year > 2010:
+        for sem in [SemesterType.SPRING, SemesterType.SUMMER, SemesterType.FALL, SemesterType.WINTER]:
+            term = NJIT.term_code(year, sem)
+            subjects = NJIT.get_subjects(term)
+            
+            db = mongo_client["NJIT_Course_API"]
+            collection = db["Courses"]
+            
+            for s in subjects:
+                subject = s['SUBJECT']
+                sections = NJIT.get_sections(term, subject)
+                print(f"[{term}]: {subject} ({len(courses)})")
+                for sec in sections:
+                    course: str = sec['COURSE']
+                    if course in courses:
                         continue
-                    
-                    print("Getting " + cname)
-                    response = session.get(f'https://dw-prod.ec.njit.edu/responsiveDashboard/api/course-link?discipline={c2[0]}&number={c2[1]}&=')
-                    time.sleep(0.25)
-                    obj = response.json()
-                    #print(session.cookies.get_dict())
+                    else:      
+                        c1 = course.replace(' ', '')
+                        c2 = split_course_code(c1)
+                        
+                        cname = " ".join(c2)
+                        
+                        if collection.find_one({'_id':cname}) != None:
+                            print("Found " + cname)
+                            courses.add(course)
+                            continue
+                        
+                        print("Getting " + cname)
+                        response = session.get(f'https://dw-prod.ec.njit.edu/responsiveDashboard/api/course-link?discipline={c2[0]}&number={c2[1]}&=')
+                        time.sleep(0.25)
+                        obj = response.json()
+                        #print(session.cookies.get_dict())
 
-                    obj['_id'] = cname
-                    obj['updated'] = datetime.date.today().strftime("%d/%m/%Y")
-                    
-                    collection.insert_one(obj)
-                    courses.add(course)
-            time.sleep(0.5)
-    year -= 1
+                        obj['_id'] = cname
+                        obj['updated'] = datetime.date.today().strftime("%d/%m/%Y")
+                        
+                        collection.insert_one(obj)
+                        courses.add(course)
+                time.sleep(0.5)
+        year -= 1
 
-print(courses)
+    print(courses)
 
 
-#Print cookies for re-use
-print(session.cookies.get_dict())
+    #Print cookies for re-use
+    print(session.cookies.get_dict())
 
--- prereq_scrape.py
+++ prereq_scrape.py
@@ -4,31 +4,8 @@ from utils import mongo_client
 import time
 import json
 
-def print_side_by_side(text, json_val: dict):
-    # Convert JSON string to a pretty-printed version
-    json_pretty = json.dumps(json_val, indent=2)
 
-    # Split both text and JSON into lines
-    text_lines = text.split('\n')
-    json_lines = json_pretty.split('\n')
-
-    # Calculate the maximum width of the text lines
-    max_text_width = max(len(line) for line in text_lines)
-
-    # Iterate over the maximum number of lines between text and JSON
-    for i in range(max(len(text_lines), len(json_lines))):
-        # Get the current line from text and JSON, or empty string if none
-        text_line = text_lines[i] if i < len(text_lines) else ''
-        json_line = json_lines[i] if i < len(json_lines) else ''
-
-        # Print the lines side by side, separated by a tab or some spaces
-        print(f"{text_line.ljust(max_text_width)}    {json_line}")
-
-
-if __name__ == "__main__":
-    # Code that looks at all professors that have taught a specific course over time
-    # Not relevant to the rest of the file, should be deleted at some point
-    
+if __name__ == "__main__":    
     courses = set()
         
     year = 2024
